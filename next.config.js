de901075f58b67f51baa54854a3a3227545714b9
Rohit <rohitgami2003@gmail.com>
2024-01-07 15:14:18 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      };
+      return config;
+    },
+    // env: {
+    //   GOOGLE_ID: process.env.GOOGLE_ID,
+    //   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    //   MONGODB_URI: process.env.MONGODB_URI,
+    //   NEXTAUTH: process.env.NEXTAUTH,
+    //   NEXTAUTH_INTERVAL: process.env.NEXTAUTH_INTERVAL,
+    //   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
+    // },
+  };
+  
+  module.exports = nextConfig;
+  
