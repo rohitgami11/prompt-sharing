e175e7d202d7b58498af2f013877f8dde59fa91e
Rohit <rohitgami2003@gmail.com>
2024-01-13 12:45:55 +0530

    fix error

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -14,7 +14,7 @@ const MyProfile = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const response = await fetch(`/api/user/${session?.user.id}/posts`);
       const data = await response.json();
 
       setMyPosts(data);
@@ -50,7 +50,7 @@ const MyProfile = () => {
   return (
     <Profile
       name='My'
-      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
       data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
